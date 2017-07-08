48b812b085b374d26aca961ed3c592f6fd0bbabc
User <user@Users-Mac-mini.local>
2017-07-09 01:56:09 +0300

    first commit

---
+++ TrainerCoach/CustomSegue.swift
@@ -0,0 +1,9 @@
+//
+//  CustomSegue.swift
+//  TrainerCoach
+//
+//  Created by User on 09/07/2017.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import Foundation
---
+++ TrainerCoach/ExercisesData.swift
@@ -0,0 +1,9 @@
+//
+//  ExercisesData.swift
+//  TrainerCoach
+//
+//  Created by User on 08/07/2017.
+//  Copyright © 2017 User. All rights reserved.
+//
+
+import Foundation
